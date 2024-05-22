0fb3db25cf58a7a1e268f7e458223f9ab1417d34
Lily Whitford <lilywhitford2@gmail.com>
2024-05-21 22:18:57 -0700

    implement functionality to display 3 PhotoListItem components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,14 +13,27 @@ const sampleDataForPhotoListItem = {
   username: "Joe Example",
   profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
 };
+const photos = new Array(3).fill(sampleDataForPhotoListItem); // This ensures the array is filled with null values, which can be helpful if you're not using the actual photo object.
 
 const App = () => {
-  
   return (
     <div className="App">
-      <PhotoListItem photo={sampleDataForPhotoListItem} />
+      {photos.map((photo, index) => ( // Since we're not using the photo variable, you can replace it with an underscore as a convention to indicate it's an unused variable.
+        <div key={index} className="photo-list"> {/* Use index as a key */}
+          <PhotoListItem photo={photo}/> {/* Removed photo prop since we don't have photo details here */}
+        </div>
+      ))}
     </div>
   );
 };
+
+// const App = () => {
+  
+//   return (
+//     <div className="App">
+//       <PhotoListItem photo={sampleDataForPhotoListItem} />
+//     </div>
+//   );
+// };
 
 export default App;
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -4,13 +4,19 @@ import "../styles/PhotoListItem.scss";
 
 
 const PhotoListItem = ({ photo }) => {
+  if (!photo) {
+    return null;
+  }
+  
   return (
     <div className="photo-list__item">
       <img className="photo-list__image" src={photo.imageSource} alt={photo.username} />
       <div className="photo-list__user-details">
         <img className="photo-list__user-profile" src={photo.profile} alt={`Profile of ${photo.username}`}/>
-        <p className="photo-list__user-info">{photo.username}</p>
-        <p className="photo-list__user-location">{photo.location.city}, {photo.location.country}</p>
+        <div className="photo-list__user-info">
+          <p>{photo.username}</p>
+          <p className="photo-list__user-location">{photo.location.city}, {photo.location.country}</p>
+        </div>
       </div>
     </div>
   );
