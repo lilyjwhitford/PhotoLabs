e58ec7eba011ee7fa1327d6ae2e222997801c949
Lily Whitford <lilywhitford2@gmail.com>
2024-05-27 10:30:23 -0700

    create modal

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,11 +4,14 @@ import './App.scss';
 import HomeRoute from './routes/HomeRoute';
 import topics from './mocks/topics';
 import photos from './mocks/photos';
+import PhotoDetailsModal from 'routes/PhotoDetailsModal';
 
 const App = () => {
+
   return (
     <div className="App">
       <HomeRoute topics={topics} photos={photos} />
+      <PhotoDetailsModal />
     </div>
   );
 };
--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import '../styles/PhotoDetailsModal.scss'
+import '../styles/PhotoDetailsModal.scss';
 import closeSymbol from '../assets/closeSymbol.svg';
 
 const PhotoDetailsModal = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  //function to open the modal
+  const openModal = () => {
+    setIsOpen(true);
+  };
+
+  //function to close the modal
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
+  // conditionally render the modal content based on its open state
   return (
-    <div className="photo-details-modal">
-      <button className="photo-details-modal__close-button">
-        <img src={closeSymbol} alt="close symbol" />
-      </button>
-    </div>
-  )
+    isOpen && (
+      <div className="photo-details-modal">
+        <button className="photo-details-modal__close-button" onClick={closeModal}>
+          <img src={closeSymbol} alt="close symbol" />
+        </button>
+      </div>
+    )
+  );
 };
 
 export default PhotoDetailsModal;
